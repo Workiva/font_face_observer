408eccf689408dfc7f87c730d693766e3b4bba2a
Rob Becker <rob.becker@workiva.com>
2016-11-29 14:16:48 -0700

    HY-4308 ok fix static dir again for smithy

--- tool/stage_for_cdn.sh
+++ tool/stage_for_cdn.sh
@@ -16,8 +16,3 @@ cp -RL build/web/packages static/
 
 # stage compiled Dart parts and source maps
 cp build/web/*dart*.+(js|js.map) static/
-
-
-# stage production HTML and JS peripherals (order is important here!)
-cp -RL build/web/js static/
-cp -RL build/web/prod/* static/
