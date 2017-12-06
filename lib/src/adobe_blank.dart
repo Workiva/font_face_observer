13bbbc51a05119b21455fdce4fd4a454ef819d94
Rob Becker <rob.becker@workiva.com>
2017-12-06 07:13:36 -0700

    Adds license info from the original font face observer JS repo and license info from the Font Face API W3C spec page.

--- lib/src/adobe_blank.dart
+++ lib/src/adobe_blank.dart
@@ -12,6 +12,53 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+This software or document includes material copied from or derived 
+from fontfaceobserver (https://github.com/bramstein/fontfaceobserver), 
+Copyright (c) 2014 - Bram Stein, which is licensed under the following terms:
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright 
+    notice, this list of conditions and the following disclaimer in the 
+    documentation and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED 
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This software or document includes material copied from or derived from 
+CSS Font Loading Module Level 3 Copyright © 2017 W3C® 
+(MIT, ERCIM, Keio, Beihang) which is licensed under the following terms:
+
+By obtaining and/or copying this work, you (the licensee) agree that you 
+have read, understood, and will comply with the following terms and conditions.
+Permission to copy, modify, and distribute this work, with or without 
+modification, for any purpose and without fee or royalty is hereby granted, 
+provided that you include the following on ALL copies of the work or portions 
+thereof, including modifications:
+The full text of this NOTICE in a location viewable to users of the 
+redistributed or derivative work. Any pre-existing intellectual property 
+disclaimers, notices, or terms and conditions. If none exist, the W3C 
+Software and Document Short Notice should be included.
+Notice of any changes or modifications, through a copyright statement 
+on the new code or document such as "This software or document 
+includes material copied from or derived from 
+[title and URI of the W3C document]. 
+Copyright © [YEAR] W3C® (MIT, ERCIM, Keio, Beihang)."
+
+https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
 */
 
 /// The CSS family name for the AdobeBlank font used in detecting
